808c872f2cc63a6fca72c8eba8a38030e6faa477
Samuel Mace <samuelcmace@gmail.com>
2024-04-10 21:04:37 -0500

    Revert Back to Old-Style Database Connection

--- database/index.js
+++ database/index.js
@@ -17,66 +17,33 @@ class DatabaseConnection {
          * a direct connection to localhost.
          * @type {string} A String representing the connection URL.
          */
-        this.mongo_connection_string = process.env.MONGO_CONNECTION_STRING;
-        if (this.mongo_connection_string === undefined) {
+        if (process.env.MONGO_CONNECTION_STRING === undefined) {
             this.mongo_connection_string = "mongodb://localhost:27017/?directConnection=true";
+        } else {
+            this.mongo_connection_string = process.env.MONGO_CONNECTION_STRING;
         }
         this.connection = new MongoClient(this.mongo_connection_string);
 
         /**
          * Environment variable representing the MongoDB Connection Timeout.
-         * @type {number} An Integer representing the number of milleseconds the
+         * @type {number} An Integer representing the number of milliseconds the MongoDB client should attempt to
+         *                connect before timing out.
          */
-        this.mongo_connection_timeout = parseInt(process.env.MONGO_CONNECTION_TIMEOUT);
-        if (this.mongo_connection_timeout === undefined) {
-            this.mongo_connection_timeout = 3000;
+        if (process.env.MONGO_CONNECTION_TIMEOUT === undefined) {
+            this.mongo_connection_timeout = 15000;
+        } else {
+            this.mongo_connection_timeout = parseInt(process.env.MONGO_CONNECTION_TIMEOUT);
         }
 
-        /**
-         * Environment variable representing the number of retries the client should attempt to connect to the server
-         * before rejecting the connection request.
-         * @type {number}
-         */
-        this.mongo_connection_retries = parseInt(process.env.MONGO_CONNECTION_RETRIES);
-        if (this.mongo_connection_retries === undefined) {
-            this.mongo_connection_retries = 3000;
-        }
-
-    }
-
-    /**
-     * Function to determine whether the database connection is up and running.
-     * @returns {boolean} A boolean determining whether the connection to the MongoDB instance is active and healthy.
-     */
-    is_connected = function () {
-        return !!this.connection && !!this.connection.topology && this.connection.isConnected();
     }
 
     /**
      * Promise-based function to establish a connection to the database.
      * @returns {Promise<unknown>} A Promise representing the outcome of the connection request.
      */
-    connect = function () {
-        return new Promise(async (resolve, reject) => {
-
-            console.log(`Connecting to MongoDB at ${this.mongo_connection_string}`);
-
-            // Attempt to connect to the database based on the maximum number of retries specified...
-            for (let retry_iteration = 0; retry_iteration < this.mongo_connection_retries; retry_iteration++) {
-                await this.connection.connect({serverSelectionTimeoutMS: this.mongo_connection_timeout});
-
-                // If the connection succeeded, break the loop...
-                if (this.is_connected()) {
-                    break;
-                }
-            }
-
-            if (this.is_connected()) {
-                resolve("Server Connection Succeeded!");
-            } else {
-                reject("Server Connection Failed!");
-            }
-        });
+    connect = async function () {
+        console.log(`Connecting to MongoDB at ${this.mongo_connection_string}`);
+        await this.connection.connect({serverSelectionTimeoutMS: this.mongo_connection_timeout});
     }
 
 }
@@ -98,22 +65,12 @@ class DBConnectionPool {
      * @returns {Promise<DatabaseConnection>} A Promise that returns the database connection when it is up and ready.
      */
     static getInstance = async function () {
-
         // If the singleton instance has not yet been instantiated, instantiate it.
         if (!DBConnectionPool.instance) {
             DBConnectionPool.instance = new DatabaseConnection();
         }
-
-        // If the database connection has not yet been established (or is unhealthy for some reason), reconnect to the
-        // database and then return the instance. Otherwise, just return the instance.
-        if(!DBConnectionPool.instance.is_connected()) {
-            await DBConnectionPool.instance.connect().then(() => {
-                return DBConnectionPool.instance;
-            });
-        } else {
-            return DBConnectionPool.instance;
-        }
-
+        await DBConnectionPool.instance.connect();
+        return DBConnectionPool.instance;
     }
 
 }
